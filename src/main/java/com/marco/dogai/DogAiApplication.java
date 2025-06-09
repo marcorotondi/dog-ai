d1a42b73671926d15f83d0e0e9dfdb4683cc8cdf
rot <marco.rotondi@intre.it>
2025-06-09 16:28:28 +0200

    init project dog-ai.

---
+++ src/main/java/com/marco/dogai/DogAiApplication.java
@@ -0,0 +1,13 @@
+package com.marco.dogai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marco/dogai/DogAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.dogai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
